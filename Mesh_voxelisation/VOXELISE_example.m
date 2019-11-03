b4f7a178273dc7e543249c39ba41ea4e65cb4d47
Jon Škerlj <jon.skerlj17@imperial.ac.uk>
2019-11-03 22:49:39 +0000

    Finished Voxel Plot of imported model

---
+++ Mesh_voxelisation/VOXELISE_example.m
@@ -0,0 +1,37 @@
+
+%Plot the original STL mesh:
+figure
+[stlcoords] = READ_stl('Lens.stl');
+xco = squeeze( stlcoords(:,1,:) )';
+yco = squeeze( stlcoords(:,2,:) )';
+zco = squeeze( stlcoords(:,3,:) )';
+[hpat] = patch(xco,yco,zco,'b');
+axis equal
+
+%Voxelise the STL:
+[OUTPUTgrid] = VOXELISE(50,100,100,'Lens.stl','xyz');
+OUTPUTgrid(51:100,:,:) = 0;
+%Show the voxelised result:
+figure;
+subplot(1,3,1);
+imagesc(squeeze(sum(OUTPUTgrid,1)));
+colormap(gray(256));
+xlabel('Z-direction');
+ylabel('Y-direction');
+axis equal tight
+
+subplot(1,3,2);
+imagesc(squeeze(sum(OUTPUTgrid,2)));
+colormap(gray(256));
+xlabel('Z-direction');
+ylabel('X-direction');
+axis equal tight
+
+subplot(1,3,3);
+imagesc(squeeze(sum(OUTPUTgrid,3)));
+colormap(gray(256));
+xlabel('Y-direction');
+ylabel('X-direction');
+axis equal tight
+
+figure (4)
--- main.m
+++ main.m
@@ -3,13 +3,14 @@ clc;
 
 filename='Lens.stl';
 [p,t,tnorm]=STL_Import(filename);
- 
-trisurf(t,p(:,1),p(:,2),p(:,3),'FaceColor',[0.5 0.5 0.5],'EdgeAlpha',0)
-axis image
-camlight
 
-figure (2)
-patch(p(:,1),p(:,2),p(:,3))
+% figure(1)
+% trisurf(t,p(:,1),p(:,2),p(:,3),'FaceColor',[0.5 0.5 0.5],'EdgeAlpha',0)
+% axis image
+% camlight
+% 
+% figure (2)
+% patch(p(:,1),p(:,2),p(:,3))
 % 
 % [v, tnorm] = STL_Import(filename, 2);
 % 
@@ -23,11 +24,11 @@ patch(p(:,1),p(:,2),p(:,3))
 
 
 
-A = [0 0 0 ; 0 1 0; 1 1 1];
-A(:,:,2) = [0 0 0 ; 0 1 0; 1 1 1];
- 
-[x,y,z] = ndgrid(1:size(A, 1), 1:size(A, 2), 1:size(A, 3)); %get coordinates of all points
-xx = x(A == 1); %keep only coordinates for A == 1 
-yy = y(A == 1); %these 3 lines also reshape the 3d array
-zz = z(A == 1); %into column vectors
-plot3(xx, yy, zz, '*'); %or whatever marker you want
+% A = [0 0 0 ; 0 1 0; 1 1 1];
+% A(:,:,2) = [0 0 0 ; 0 1 0; 1 1 1];
+%  
+% [x,y,z] = ndgrid(1:size(A, 1), 1:size(A, 2), 1:size(A, 3)); %get coordinates of all points
+% xx = x(A == 1); %keep only coordinates for A == 1 
+% yy = y(A == 1); %these 3 lines also reshape the 3d array
+% zz = z(A == 1); %into column vectors
+% plot3(xx, yy, zz, '*'); %or whatever marker you want
